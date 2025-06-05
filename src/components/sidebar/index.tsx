5075973a8b33a3a081e0de5507f82a0d7fa1eb94
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2025-06-05 22:10:33 +0600

    feat(dependencies): add Radix UI components and utility libraries; update styles and global CSS variables

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import {
+  AudioWaveform,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  ListTodo,
+  Map,
+  MessageCircle,
+  PieChart,
+  Users2,
+} from "lucide-react";
+import * as React from "react";
+
+import { NavMain } from "@/components/sidebar/nav-main";
+import { NavUser } from "@/components/sidebar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+import { WorkspaceSwitcher } from "./workspace-switcher";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Messenger",
+      url: "/dashboard",
+      icon: MessageCircle,
+      isActive: true,
+    },
+    {
+      title: "Tasks",
+      url: "/tasks",
+      icon: ListTodo,
+    },
+    {
+      title: "Contacts",
+      url: "/contacts",
+      icon: Users2,
+    },
+  ],
+
+  projects: [
+    {
+      name: "Design Engineering",
+      url: "#",
+      icon: Frame,
+    },
+    {
+      name: "Sales & Marketing",
+      url: "#",
+      icon: PieChart,
+    },
+    {
+      name: "Travel",
+      url: "#",
+      icon: Map,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <WorkspaceSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
